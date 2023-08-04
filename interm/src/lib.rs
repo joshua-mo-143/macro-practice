83c9e3eaab4601f148e9533784d28f2ee3f475d9
Joshua Mo <joshua.mo.876@gmail.com>
2023-08-04 23:19:29 +0100

    feat: mojibake macro

---
+++ interm/src/lib.rs
@@ -0,0 +1,2 @@
+pub use encoding_rs::SHIFT_JIS;
+pub use macros::mojibake;
--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,6 +1,12 @@
-use proc_macro::{TokenStream, Span};
-use syn::{ItemFn, Ident};
-use quote::{quote_spanned};
+use proc_macro::{Group, Span, TokenStream};
+use quote::quote_spanned;
+use std::collections::HashSet;
+use syn::{
+    parse::{Parse, ParseStream, Result},
+    parse_macro_input,
+    punctuated::Punctuated,
+    Data, DeriveInput, Ident, ItemFn, Token,
+};
 
 #[proc_macro_attribute]
 pub fn benchmark(_args: TokenStream, input: TokenStream) -> TokenStream {
@@ -16,7 +22,7 @@ pub fn benchmark(_args: TokenStream, input: TokenStream) -> TokenStream {
     let code = quote_spanned! {span=>
         fn #name_as_ident() {
         let time = std::time::Instant::now();
-            
+
         println!("The name of this function is: {}", #name);
             #code
         println!("Amount of time taken for this fn to complete: {:?}", time.elapsed());
@@ -25,4 +31,73 @@ pub fn benchmark(_args: TokenStream, input: TokenStream) -> TokenStream {
     };
 
     TokenStream::from(code)
+}
+
+#[proc_macro_attribute]
+pub fn mojibake(args: TokenStream, input: TokenStream) -> TokenStream {
+    let meme = parse_macro_input!(args as Args);
+
+    let input = syn::parse2::<DeriveInput>(input.into())
+        .expect("Couldn't parse into DeriveInput, are you using this on a Struct?");
+
+    let struct_name = input.ident;
+
+    let span = Span::mixed_site().into();
+
+    let data = match input.data {
+        Data::Struct(data_struct) => data_struct,
+        _ => {
+            panic!("This isn't a struct!");
+        }
+    };
+
+    let fields = data.fields.clone();
+
+    let field_names: Vec<_> = data
+        .fields
+        .iter()
+        .map(|x| x.ident.clone().unwrap())
+        .collect();
+
+    let field_names_to_mojibake = field_names.iter().map(|ref x| {
+        Ident::new(
+            format!("{}_to_mojibake", x).as_str(),
+            Span::call_site().into(),
+        )
+    });
+
+    let code = quote_spanned! {span=>
+        struct #struct_name
+            #fields
+
+        impl #struct_name {
+            #(
+                fn #field_names_to_mojibake(&self) -> String {
+                    let str = &self.#field_names.to_string();
+                    let (res, encoding, had_errors) = SHIFT_JIS.encode(&str);
+
+                    let vec: Vec<u8> = res.into_owned().to_vec().iter().map(|x| x + 75).collect();
+
+                    let (data, encoding, had_errors) = SHIFT_JIS.decode(&vec);
+
+                    data.into_owned().to_string()
+                }
+            )*
+        }
+    };
+
+    TokenStream::from(code)
+}
+
+struct Args {
+    vars: HashSet<Ident>,
+}
+
+impl Parse for Args {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let vars = Punctuated::<Ident, Token![,]>::parse_terminated(input)?;
+        Ok(Args {
+            vars: vars.into_iter().collect(),
+        })
+    }
 }
